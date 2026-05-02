d61a1348f7a68a1696ec4ea8891a4adc28f78e48
Kowsik <kowsikskowsiks68@gmail.com>
2026-05-02 14:31:28 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 8080;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "dist")));
+
+// React routing fix
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Frontend running on port ${PORT}`);
+});
