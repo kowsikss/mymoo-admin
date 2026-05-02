06facbe914beed51b2a59810f6e859b38df9056d
Kowsik <kowsikskowsiks68@gmail.com>
2026-05-02 14:58:49 +0530

    changes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: process.env.PORT || 5173,
+    allowedHosts: [
+      'mymoo-admin-production.up.railway.app'
+    ]
+  }
 })
